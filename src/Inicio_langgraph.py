620c21b93d25727248dd9442588a04ecdca43b40
Arthur-Murilo <arthur.msc1811@hotmail.com>
2026-03-22 20:40:36 -0300

    Refactor code structure for improved readability and maintainability

--- src/Inicio_langgraph.py
+++ src/Inicio_langgraph.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
 
 load_dotenv()
 
-model = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
-    temperature=0.7,  # Gemini 3.0+ defaults to 1.0
+model = ChatGroq(
+    model="openai/gpt-oss-120b",
+    temperature=0.7,
     max_tokens=None,
     timeout=None,
     max_retries=2,
@@ -17,7 +17,7 @@ messages = [
         "system",
         "Você é um assistente bem legal por sinal",
     ),
-    ("human", "OPa tudo bem, em respodne em portugues tá?"),
+    ("human", "Opa tudo bem, em respodne em portugues tá?"),
 ]
 ai_msg = model.invoke(messages)
 print(ai_msg.content)
